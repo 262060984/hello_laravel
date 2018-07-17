b82a71fcd190a5a7a706750c8e56e93d42542a70
vagrant <vagrant@homestead>
2018-07-17 09:19:34 +0000

    111

---
+++ resources/views/test.blade.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>Laravel 入门教程</title>
+    <link rel="stylesheet" href="/css/app.css">
+  </head>
+  <body>
+<li><a href="{{ route('help') }}">帮助</a></li>
+</body>
+</html>
